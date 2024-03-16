b70bbef96be1ca93a35c5171b798ef346e700848
YD1RUH <32731955+YD1RUH@users.noreply.github.com>
2024-03-17 05:55:16 +0700

    Add files via upload

---
+++ tx_fm.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: transmit using hackrf
+# Author: YD1RUH
+# GNU Radio version: 3.10.4.0
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+import argparse
+import os
+
+parser = argparse.ArgumentParser(prog=None,description="input freq")   
+parser.add_argument("--freq", nargs="?",help="option")
+args = parser.parse_args()
+svc_name = "freq"
+freq_tx = int(vars(args).get(svc_name))
+
+class tx_fm(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "transmit using hackrf", catch_exceptions=True)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 48e3
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=2000000,
+                decimation=96000,
+                taps=[],
+                fractional_bw=0)
+        self.osmosdr_sink_0 = osmosdr.sink(
+            args="numchan=" + str(1) + " " + 'hackrf'
+        )
+        self.osmosdr_sink_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_sink_0.set_sample_rate(2000000)
+        self.osmosdr_sink_0.set_center_freq(freq_tx, 0)
+        self.osmosdr_sink_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0.set_gain(10, 0)
+        self.osmosdr_sink_0.set_if_gain(20, 0)
+        self.osmosdr_sink_0.set_bb_gain(20, 0)
+        self.osmosdr_sink_0.set_antenna('', 0)
+        self.osmosdr_sink_0.set_bandwidth(12500, 0)
+        current = os.getcwd()
+        self.blocks_wavfile_source_0 = blocks.wavfile_source(current+'/send.wav', False)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(1)
+        self.analog_nbfm_tx_0 = analog.nbfm_tx(
+        	audio_rate=48000,
+        	quad_rate=96000,
+        	tau=(75e-6),
+        	max_dev=5e3,
+        	fh=0,
+                )
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_nbfm_tx_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.analog_nbfm_tx_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.osmosdr_sink_0, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+
+
+
+def main(top_block_cls=tx_fm, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
