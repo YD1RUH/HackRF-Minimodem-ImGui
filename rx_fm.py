9676097b4ba27056c1c144e5b2a176b8b20b7338
YD1RUH <32731955+YD1RUH@users.noreply.github.com>
2024-03-21 22:53:02 +0700

    Add files via upload
    adding tunner for tunning gain HackRF

--- rx_fm.py
+++ rx_fm.py
@@ -7,22 +7,30 @@
 # GNU Radio Python Flow Graph
 # Title: receive using hackrf
 # Author: yd1ruh
-# GNU Radio version: 3.10.4.0
+# GNU Radio version: 3.10.9.2
 
+from PyQt5 import Qt
+from gnuradio import qtgui
+from PyQt5 import QtCore
 from gnuradio import analog
 from gnuradio import audio
+from gnuradio import blocks
 from gnuradio import filter
 from gnuradio.filter import firdes
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import osmosdr
 import time
+import sip
+import subprocess
 import argparse
+import os
 
 parser = argparse.ArgumentParser(prog=None,description="input freq")   
 parser.add_argument("--freq", nargs="?",help="option")
@@ -30,19 +38,68 @@ args = parser.parse_args()
 svc_name = "freq"
 freq_rx = int(vars(args).get(svc_name))
 
-class rx_fm(gr.top_block):
+class rx_fm(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "receive using hackrf", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("receive using hackrf")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "rx_fm")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
 
         ##################################################
         # Variables
         ##################################################
+        self.vol = vol = 0.3
+        self.sql = sql = -50
         self.samp_rate = samp_rate = 192000
+        self.rf_gain = rf_gain = 38
+        self.if_gain = if_gain = 16
+        self.freq = freq = freq_rx
+        self.bb_gain = bb_gain = 28
 
         ##################################################
         # Blocks
         ##################################################
+
+        self._vol_range = qtgui.Range(0.1, 1, 0.01, 0.3, 50)
+        self._vol_win = qtgui.RangeWidget(self._vol_range, self.set_vol, "vol", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._vol_win)
+        self._sql_range = qtgui.Range(-100, 50, 0.1, -50, 50)
+        self._sql_win = qtgui.RangeWidget(self._sql_range, self.set_sql, "sql", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._sql_win)
+        self._rf_gain_range = qtgui.Range(1, 60, 1, 38, 50)
+        self._rf_gain_win = qtgui.RangeWidget(self._rf_gain_range, self.set_rf_gain, "rf_gain", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._rf_gain_win)
+        self._if_gain_range = qtgui.Range(1, 60, 1, 16, 50)
+        self._if_gain_win = qtgui.RangeWidget(self._if_gain_range, self.set_if_gain, "if_gain", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._if_gain_win)
+        self._bb_gain_range = qtgui.Range(1, 60, 1, 28, 50)
+        self._bb_gain_win = qtgui.RangeWidget(self._bb_gain_range, self.set_bb_gain, "bb_gain", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._bb_gain_win)
         self.rational_resampler_xxx_1 = filter.rational_resampler_fff(
                 interpolation=6,
                 decimation=10,
@@ -53,19 +110,55 @@ class rx_fm(gr.top_block):
                 decimation=100,
                 taps=[],
                 fractional_bw=0)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+            1024, #size
+            window.WIN_HAMMING, #wintype
+            freq, #fc
+            48e3, #bw
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(False)
+
+        self.qtgui_waterfall_sink_x_0.disable_legend()
+
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
         self.osmosdr_source_0 = osmosdr.source(
             args="numchan=" + str(1) + " " + 'hackrf'
         )
         self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_source_0.set_sample_rate(8e6)
-        self.osmosdr_source_0.set_center_freq(freq_rx, 0)
+        self.osmosdr_source_0.set_center_freq((freq+5000), 0)
         self.osmosdr_source_0.set_freq_corr(0, 0)
         self.osmosdr_source_0.set_dc_offset_mode(0, 0)
         self.osmosdr_source_0.set_iq_balance_mode(0, 0)
         self.osmosdr_source_0.set_gain_mode(False, 0)
-        self.osmosdr_source_0.set_gain(20, 0)
-        self.osmosdr_source_0.set_if_gain(40, 0)
-        self.osmosdr_source_0.set_bb_gain(40, 0)
+        self.osmosdr_source_0.set_gain(rf_gain, 0)
+        self.osmosdr_source_0.set_if_gain(if_gain, 0)
+        self.osmosdr_source_0.set_bb_gain(bb_gain, 0)
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(15000, 0)
         self.low_pass_filter_0 = filter.fir_filter_ccf(
@@ -77,10 +170,13 @@ class rx_fm(gr.top_block):
                 100,
                 window.WIN_HAMMING,
                 6.76))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(vol)
+        self.blocks_correctiq_0 = blocks.correctiq()
         self.audio_sink_0 = audio.sink(48000, 'pulse', True)
+        self.analog_simple_squelch_cc_0 = analog.simple_squelch_cc(sql, 1)
         self.analog_nbfm_rx_0 = analog.nbfm_rx(
-        	audio_rate=80000,
-        	quad_rate=80000,
+        	audio_rate=48000,
+        	quad_rate=48000,
         	tau=(75e-6),
         	max_dev=5e3,
           )
@@ -90,11 +186,37 @@ class rx_fm(gr.top_block):
         # Connections
         ##################################################
         self.connect((self.analog_nbfm_rx_0, 0), (self.rational_resampler_xxx_1, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.analog_nbfm_rx_0, 0))
-        self.connect((self.osmosdr_source_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.analog_simple_squelch_cc_0, 0), (self.analog_nbfm_rx_0, 0))
+        self.connect((self.blocks_correctiq_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.blocks_correctiq_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_simple_squelch_cc_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_correctiq_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.rational_resampler_xxx_1, 0), (self.audio_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.blocks_multiply_const_vxx_0, 0))
 
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "rx_fm")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_vol(self):
+        return self.vol
+
+    def set_vol(self, vol):
+        self.vol = vol
+        self.blocks_multiply_const_vxx_0.set_k(self.vol)
+
+    def get_sql(self):
+        return self.sql
+
+    def set_sql(self, sql):
+        self.sql = sql
+        self.analog_simple_squelch_cc_0.set_threshold(self.sql)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -102,30 +224,62 @@ class rx_fm(gr.top_block):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
 
+    def get_rf_gain(self):
+        return self.rf_gain
+
+    def set_rf_gain(self, rf_gain):
+        self.rf_gain = rf_gain
+        self.osmosdr_source_0.set_gain(self.rf_gain, 0)
+
+    def get_if_gain(self):
+        return self.if_gain
+
+    def set_if_gain(self, if_gain):
+        self.if_gain = if_gain
+        self.osmosdr_source_0.set_if_gain(self.if_gain, 0)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.osmosdr_source_0.set_center_freq((self.freq+5000), 0)
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, 48e3)
+
+    def get_bb_gain(self):
+        return self.bb_gain
+
+    def set_bb_gain(self, bb_gain):
+        self.bb_gain = bb_gain
+        self.osmosdr_source_0.set_bb_gain(self.bb_gain, 0)
+
 
 
 
 def main(top_block_cls=rx_fm, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
     tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        sys.exit(0)
+        Qt.QApplication.quit()
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    tb.start()
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
 
-    try:
-        input('Press Enter to quit: ')
-    except EOFError:
-        pass
-    tb.stop()
-    tb.wait()
-
+    qapp.exec_()
 
 if __name__ == '__main__':
     main()
